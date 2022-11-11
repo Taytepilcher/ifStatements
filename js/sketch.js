1cd0271525fb4946201a7d98f8cb7719806a07f8
Taytepilcher <91669192+Taytepilcher@users.noreply.github.com>
2022-11-11 10:53:44 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,28 @@
 function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
+    textSize(20)
+    var flag = true
 }
 
 function draw() {
     background(220);
-
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    fill(255, 255, 0)
+    circle(width/2,height/2,200)
+    fill(255, 0,0)
+    circle(365, 375 , 50)
+    fill(255, 0,0)
+    circle(425, 375 , 50)
+    arc(50, 50, 80, 80, 0, PI + QUARTER_PI, PIE);
+if (mouseX < width/2)
+{
+    fill(mouseX/2)
+text('left',mouseX, height/2)
 }
+else{
+    fill(mouseX/2)
+text('right' ,mouseX, height/2)
+    }
+console.log(mouseX)
+}
+
